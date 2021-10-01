811070fd34e4453b2d30aed626a15e5aa4cc147b
Your Name <advmenendez92@gmail.com>
2021-10-01 17:40:28 +0200

    Iteration#3

---
+++ Iteration3.js
@@ -0,0 +1,26 @@
+//1.1
+let a = 10;
+let b = 5;
+console.log(a*b)
+
+//1.2 
+let c =10
+let d = 2
+console.log(c/d)
+
+//1.3 
+let e = 15;
+let f = 9;
+console.log(e/f)
+
+//1.4 
+let y =10;
+let z = 5;
+let x = (y + z);
+console.log(x)
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables y = 10 y z = 5
+//let y = 10;
+//let z = 5;
+//let x =(y*z)
+//console.log(x)
