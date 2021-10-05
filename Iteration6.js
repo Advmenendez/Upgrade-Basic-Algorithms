ffc67b3d19460f071d268e9152f696cec56dbbfb
Your Name <advmenendez92@gmail.com>
2021-10-05 14:53:16 +0200

    second commit

--- Iteration6.js
+++ Iteration6.js
@@ -10,7 +10,7 @@ for (let i = 0; i <= 9; i++) {
 //1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
 //cuando el resto del numero dividido entre 2 sea 0.
 for (let i = 0; i <= 10; i++) {
-    if ((i/2)==0)
+    if ((i%2)==0)
     console.log(i)
 }
 /* 1.3 Crea un bucle para conseguir dormir contando ovejas. 
